8b2da41d2159427879f27aae164c64745f7468c8
Biltu Das <65584801+BiltuDas1@users.noreply.github.com>
2021-06-08 13:00:09 +0530

    Linux (qBittorrent v2)
    Fixed Integration with Store version of qBittorrent

--- Linux/qBittorrent/torrent.sh
+++ Linux/qBittorrent/torrent.sh
@@ -1,1 +1,6 @@
-rm -r "/home/$USER/.local/share/qBittorrent/BT_backup"
+if [ -d "/home/$USER/.local/share/qBittorrent" ] 
+then
+    rm -r "/home/$USER/.local/share/qBittorrent/BT_backup"
+else
+    rm -r "/home/$USER/snap/qbittorrent-arnatious/83/.local/share/qBittorrent/BT_backup"
+fi
